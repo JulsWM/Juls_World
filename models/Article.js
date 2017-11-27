2553e01274e9928ecd4846b6d176c1cdd9ed01df
Julia Wolf Martinez <juliahmartinez@yahoo.com>
2017-11-27 15:30:58 -0700

    made changes to code

---
+++ models/Article.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    body: Srting
+  },
+  { timestamps: true }
+);
+const Article = mongoose.model("Article", ArticleSchema);
+module.exports = Article;
